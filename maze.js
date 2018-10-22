375b107a74a0fd2789a01962fa8e5bb2000cf7ff
Rogon101 <rogonjones@gmail.com>
2018-10-22 16:08:42 -0500

    All boundaries glow red on hover

--- maze.js
+++ maze.js
@@ -1,8 +1,19 @@
 window.onload = ()=> {
-    document.getElementById("boundary1").onmouseover = overBoundary ;
+    document.getElementById("boundary1").onmouseover = overBoundary1;
+    let boundaries = document.querySelectorAll("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].onmouseover = overBoundary;
+    }
 };
 
-function overBoundary() {
+function overBoundary1() {
     let boundary = document.querySelector("div#maze div.boundary");
     boundary.classList.add("youlose");
+}
+
+function overBoundary() {
+    let boundaries = document.querySelectorAll("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].classList.add("youlose");
+    }
 }
