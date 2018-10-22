9e9a474e0b00acf3b855bf953a9bb40381825619
Rogon101 <rogonjones@gmail.com>
2018-10-22 17:01:44 -0500

    Alert on successful completion of maze

--- maze.js
+++ maze.js
@@ -1,19 +1,37 @@
+var lose = false;
+
 window.onload = ()=> {
     document.getElementById("boundary1").onmouseover = overBoundary1;
+    document.getElementById("end").onmouseover = atEnd;
     let boundaries = document.querySelectorAll("div#maze div.boundary");
     for (var i = 0; i < boundaries.length; i++) {
+
         boundaries[i].onmouseover = overBoundary;
     }
+    
 };
 
 function overBoundary1() {
+    lose = true;
     let boundary = document.querySelector("div#maze div.boundary");
     boundary.classList.add("youlose");
 }
 
 function overBoundary() {
+    lose = true;
     let boundaries = document.querySelectorAll("div#maze div.boundary");
     for (var i = 0; i < boundaries.length; i++) {
+
         boundaries[i].classList.add("youlose");
+    } 
+}
+
+function atEnd() {
+    if(lose) {
+
+        document.getElementById("status").innerHTML = "You lost....";
+    } else {
+        
+        document.getElementById("status").innerHTML = "You win!!! ";
     }
 }
